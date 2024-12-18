3cbc9070481a3e69875be360d576746552b19617
leeheekwon <heekwon0867@naver.com>
2024-12-19 02:28:24 +0900

    [fix] frontend 환경분리

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -4,7 +4,7 @@ import HomePage from '@/views/MainPage.vue';
 import {useAuthStore} from "@/stores/auth.ts";
 
 const router = createRouter({
-    history: createWebHistory(/*import.meta.env.BASE_URL*/),
+    history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
         {
             path: '/',
