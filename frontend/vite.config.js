a5b572235a194af510fa2d696347a8ea0b549e2f
leeheekwon <heekwon0867@naver.com>
2024-12-19 02:35:49 +0900

    [fix] frontend 환경분리

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,17 +3,19 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': path.resolve(__dirname, './src'),
     },
+  },
+  base: '/',
+  build: {
+    outDir: 'dist',
+    sourcemap: true
   },
   server: {
     port: 5173,  // 포트 번호를 5173으로 설정
