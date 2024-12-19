ccafebd5d852b6b055677ceec90515b23517cf20
leeheekwon <heekwon0867@naver.com>
2024-12-19 12:52:15 +0900

    [test] backend-deploy

--- backend/src/main/java/gg/updown/backend/config/JasyptConfigDES.java
+++ backend/src/main/java/gg/updown/backend/config/JasyptConfigDES.java
@@ -26,6 +26,7 @@ public class JasyptConfigDES {
         config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator"); // salt 생성 클래스
         config.setStringOutputType("base64"); //인코딩 방식
         encryptor.setConfig(config);
+        System.out.println("JasyptConfigDES Bean");
         return encryptor;
     }
 }
