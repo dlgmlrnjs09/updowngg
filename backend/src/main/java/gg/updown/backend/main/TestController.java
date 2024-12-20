ef18a51e9f1fe1091e386dfad2bdbd6466ddc551
leeheekwon <heekwon0867@naver.com>
2024-12-21 00:01:37 +0900

    [set] 라이엇 토큰교체

--- backend/src/main/java/gg/updown/backend/main/TestController.java
+++ backend/src/main/java/gg/updown/backend/main/TestController.java
@@ -23,21 +23,11 @@ public class TestController {
 
     @GetMapping("/test")
     public void test() {
-//        AccountInfoResDto resDto = accountApiService.getAccountInfoByRiotId("조용조용조용필", "KR2");
-//        riotAccountService.insertAccountBasicInfo(RiotAccountInfoEntity.builder()
-//            .puuid(resDto.getPuuid())
-//            .gameName(resDto.getGameName())
-//            .tagLine(resDto.getTagLine())
-//        .build());
+    }
 
-//        List<LolMatchInfoResDto> list = matchService.getAndInsertMatchList("W8BbbIDV3ctMEWIklMFx0sFk6yXmTfBfuLYUdglXhwG9LPdrxc5ITkDCxaOo_vJKbWm3haDdPlDNBw", 1, 20);
-//        System.out.println("");
-
-        System.out.println("db-url = " + jasyptEncoding("jdbc:log4jdbc:postgresql://13.124.117.180:5432/updowngg"));
-        System.out.println("db-username = " + jasyptEncoding("dlgmlrnjs09"));
-        System.out.println("db-password = " + jasyptEncoding("fkflfk123"));
-        System.out.println("riot-api-key = " + jasyptEncoding("RGAPI-84f64606-b562-4110-b337-c47c6aa105dd"));
-        System.out.println("jwt-secret = " + jasyptEncoding("e4f569456859c1789f80184d8c89bce1e04a9e3c0321aa166b3bde52b69fcc1d"));
+    @GetMapping("/jasyptEncoding")
+    public void getJasyptEncoding(String value) {
+        System.out.println(jasyptEncoding(value));
     }
 
     public String jasyptEncoding(String value) {
