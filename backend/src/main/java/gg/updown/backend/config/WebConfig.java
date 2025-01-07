0b0863e5a88bd8a0abcd3438ca584c91c44edaf1
leeheekwon <heekwon0867@naver.com>
2025-01-07 16:48:47 +0900

    [fix] favicon 관련 에러 출력되지 않도록 설정

--- backend/src/main/java/gg/updown/backend/config/WebConfig.java
+++ backend/src/main/java/gg/updown/backend/config/WebConfig.java
@@ -2,6 +2,7 @@ package gg.updown.backend.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -21,5 +22,11 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .maxAge(3600);
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**")
+                .addResourceLocations("classpath:/static/");
     }
 }
