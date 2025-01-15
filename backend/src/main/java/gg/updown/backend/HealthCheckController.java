c587e490c84d7c403e329ef30d95b2502484a841
leeheekwon <heekwon0867@naver.com>
2025-01-15 21:47:11 +0900

    [set] ALB 헬스체크 컨트롤러 수정

--- backend/src/main/java/gg/updown/backend/HealthCheckController.java
+++ backend/src/main/java/gg/updown/backend/HealthCheckController.java
@@ -4,9 +4,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController("/api/v1/health")
 public class HealthCheckController {
-    @GetMapping("/health")
+    @GetMapping("/check")
     public ResponseEntity<String> healthCheck() {
         return ResponseEntity.ok("OK");
     }
